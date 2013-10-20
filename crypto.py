89886d0dd678693167a91890d9da5a746c87fe9f
Anuj Gupta <anuj@codesyrup.com>
2013-10-21 00:53:59 +0530

    Minor refactoring, fixed epoch calculation

--- crypto.py
+++ crypto.py
@@ -3,19 +3,15 @@
 An early "lightning" attempt at getting TeleHash crypto working in Python
 """
 
-DEST_HASH = '7714027e51684b4109b8a739f49d21a7b392351e8029fbda3e4020d3be2d8029'
-DEST_KEY = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAteQs3n6f4OE7fre68VMv\nmrjFT8f1eN+1941BK+GwgCyl++fWXHZIvfWEakFQzMlBPSf0sP04mjCMTIBZH3VG\nOkE1lrkBPz24jlTl2SJzWeBhEfRPZCz4FEMQ6prToJSEYxZsmlcL3UDFW9dFPokc\n/HEJYx8PgTy1Q7+OVKbDyhvKx1hd9rRoatVPcszksRrz4A+nAbUTg8+VW61LJaka\nKKVmSxHoOUGXZ2DjE/l1/Ups0EAUAxDdt3lsZoxZL9an2Obq2YXnBTWT3GEogSRp\nwMz2eDufkVE4iOPz9q7DsYrmGFiPlY6DG0bq2K/rFInReFzTrC5/7WtIZGrebhqo\nzwIDAQAB\n-----END PUBLIC KEY-----\n"
-
+from local_vars import DEST_KEY, DEST_HASH
 import os
 from struct import pack, unpack
 from tomcrypt import rsa, ecc, cipher, hash
+import time
 
-import datetime
 def epoch_milli():
-    epoch = datetime.datetime.utcfromtimestamp(0)
-    now = datetime.datetime.now()
-    delta = now - epoch
-    return int(delta.total_seconds() * 1000)
+    return int(time.time() * 1000)
+
 
 try: import simplejson as json
 except ImportError: import json
@@ -27,9 +23,11 @@ except:
         id_key = rsa.Key(2048)
         f.write(id_key.as_string())
 
+
 id_key_pub = id_key.public.as_string(format='der')
 
 dest_key = rsa.Key(DEST_KEY)
+
 session_ecc = ecc.Key(256)
 session_ecc_pub = session_ecc.public.as_string(format='der', ansi=True)
 
@@ -41,6 +39,8 @@ inner_open['to'] = DEST_HASH
 inner_open['at'] = epoch_milli()
 inner_open['line'] = line_id.encode('hex')
 
+# TODO: refactor into packet encoding function
+# ---------------
 # defaults to utf-8
 inner_open_json = json.dumps(inner_open, separators=(',', ':'), sort_keys=True)
 
@@ -48,9 +48,9 @@ inner_len = len(inner_open_json)
 id_key_len = len(id_key_pub)
 
 # magical C string packing
-# TODO: refactor into packet encoding function
 fmt_str = '!H' + str(inner_len) + 's' + str(id_key_len) + 's'
 inner_open_packet = pack(fmt_str, inner_len, inner_open_json, id_key_pub)
+# ---------------
 
 hasher = hash.new('sha256', session_ecc_pub)
 sym_key = cipher.aes(key=hasher.digest(), iv=iv, mode='ctr')
@@ -61,10 +61,11 @@ outer_open['type'] = 'open'
 outer_open['open'] = dest_key.encrypt( #encrypted to recipient
             session_ecc.public.as_string(format='der',ansi=True) #ANSI X9.63
             ).encode('base64').translate(None, '\n')
+outer_open['iv'] = iv.encode('hex')
 
+hasher = hash.new('sha256', session_ecc_pub)
 hasher.update(line_id)
 sym_key = cipher.aes(key=hasher.digest(), iv=iv, mode='ctr')
-outer_open['iv'] = iv.encode('hex')
 """
 The default padding for pytomcrypt's rsa sign() is pss. The node implementation
 uses the older PKCS1 padding ('v1.5' in pytomcrypt) but that setting segfaults
